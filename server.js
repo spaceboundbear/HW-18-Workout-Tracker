9f4499685ec84fa0765c313684149b31271b8200
spaceboundbear <mf.michaelfischer@gmail.com>
2021-11-27 17:23:07 -0600

    server file boilerplate

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const router = require('./routes/api.js');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/HW18', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(require('./routes/api.js'));
+app.use(require('./routes/html.js'));
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}!`);
+});
