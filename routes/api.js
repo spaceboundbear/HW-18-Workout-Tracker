927ee918f4a48997d5fc4e7125852be1e7a265cb
spaceboundbear <mf.michaelfischer@gmail.com>
2021-11-28 14:09:00 -0600

    updating routes

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,69 @@
-//
+const router = require('express').Router();
+const Workout = require('../models/workouts');
+const { route } = require('./html');
+
+router.get('/api/workouts', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalD: {
+          $sum: '$excercise.duration',
+        },
+      },
+    },
+  ])
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post('/api/workouts', (req, res) => {
+  Workout.create({})
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercise: req.body } },
+    { new: true, runValidators: true }
+  )
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get('/api/workoutes/range', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalD: {
+          $sum: '$exercise.duration',
+        },
+        totalW: {
+          $sum: '$exercise.weight',
+        },
+      },
+    },
+  ])
+    .limit(20)
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
--- routes/html.js
+++ routes/html.js
@@ -1,1 +1,20 @@
-//
+const express = require('express');
+const path = require('path');
+const router = require('express').Router();
+
+//homepage
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+//exercise page
+router.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+//stats page
+router.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+module.exports = router;
